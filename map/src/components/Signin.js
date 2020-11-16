1ef04bc85808c8245d8f8dece135762361f3bbb9
Nguyễn Lê Hoàng Nam <68092882+nlhnam2000@users.noreply.github.com>
2020-11-16 14:25:48 +0700

    undo signin page

--- map/src/components/Signin.js
+++ map/src/components/Signin.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../App.css'
 import avatar from '../images/male_ava.png'
+import bg from '../images/bg.svg'
 
 // const background = {
 //     opacity: '0.9'
@@ -14,27 +15,12 @@ class SignIn extends React.Component {
     render() {
         return (
             <div className="signin-section">
-                <div className="signin-box">
-                    <div className="welcome">
-                        <h1 className="text-center text-uppercase">Sign in</h1>
+                <div className="container-fluid">
+                    <div className="background-img">
+                        <img src={bg} />
                     </div>
-                    <div className="side">
-                        <div className="left-side">
-                            <img src={avatar} />
-                        </div>
-                        <div className="right-side">
-                            <form method="POST">
-                                <div className="input-username">
-                                    <div className="icon">
-                                        <i className="fas fa-user"></i>
-                                    </div>
-                                    <div className="info">
-                                        <h5>Username</h5>
-                                        <input type="text" className="input" />
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                    <div className="">
+
                     </div>
                 </div>
             </div>
