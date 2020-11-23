cea62c3e8dbf28927982c8ee4a869aa00d387c68
khoado24 <khoado24@gmail.com>
2020-11-23 23:47:05 +0700

    upd icon

--- frontend/src/components/VenueLocationIcon.js
+++ frontend/src/components/VenueLocationIcon.js
@@ -1,12 +1,12 @@
 import L from 'leaflet';
-
-export const VenueLocationIcon = L.icon({
-  iconUrl: require('../images/venue_location_icon.svg'),
-  iconRetinaUrl: require('../images/venue_location_icon.svg'),
+import icon from '../images/venue_location_icon.svg'
+export const VenueLocationIcon = new L.icon({
+  iconUrl: icon,
+  iconRetinaUrl: icon,
   iconAnchor: null,
   shadowUrl: null,
   shadowSize: null,
   shadowAnchor: null,
-  iconSize: [35, 35],
+  iconSize: new L.Point(35, 35),
   className: 'leaflet-venue-icon'
 });
