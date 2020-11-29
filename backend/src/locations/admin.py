6ad56aae8cb5f44ef93af087ceaa71a823d8b85c
Kyler <kylernguyen0512@gmail.com>
2020-11-29 22:49:25 +0700

    Update db

--- backend/src/locations/admin.py
+++ backend/src/locations/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from .models import Location
+from .models import User
+from .models import Rent_Detail
+from .models import Station
 
 admin.site.register(Location)
+admin.site.register(User)
+admin.site.register(Rent_Detail)
+admin.site.register(Station)
