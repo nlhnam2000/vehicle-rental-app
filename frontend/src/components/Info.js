acc73c7c47bf969f7bf4a68ae0bcbbbbc1fb9ece
Nam <hoangnam12c12@gmail.com>
2021-01-07 17:56:52 +0700

    print history

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 import avatar from '../images/male_ava.png';
+import { Button, Modal } from 'react-bootstrap'
 
 export default class Info extends React.Component {
 
@@ -15,7 +16,8 @@ export default class Info extends React.Component {
             cmnd: '',
             point: '',
             money: '',
-            history: '',
+            history: [],
+            showModal: false
         }
     }
 
@@ -30,12 +32,20 @@ export default class Info extends React.Component {
                     cmnd: res.data.cmnd,
                     point: res.data.pointReward,
                     money: res.data.money,
-                    //history: res.data.history
+                    history: res.data.history
                 })
                 // console.log(this.state);
                 // console.log(this.state.username);
             })
             .catch(err => console.log(err))
+    }
+
+    closeModal() {
+        this.setState({ showModal: false })
+    }
+
+    openModal() {
+        this.setState({ showModal: true })
     }
 
     render() {
@@ -90,14 +100,29 @@ export default class Info extends React.Component {
                                         <h4>{this.state.money}</h4>
                                     </div>
                                 </div>
-                                <div className="area">
-                                    <div className="field">
-                                        <h4>Histoire de traverser: </h4>
-                                    </div>
-                                    <div className="data">
-                                        <h4>{this.state.history}</h4>
-                                    </div>
-                                </div>
+                                <Button onClick={this.openModal.bind(this)}>Voir histoire</Button>
+                                <Modal show={this.state.showModal} onHide={this.closeModal.bind(this)}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title>Votre histoire</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body style={{ overflow: 'scroll', height: '600px', }}>
+                                        {
+                                            this.state.history.map((item, index) => (
+                                                <div style={{ borderBottom: '1px solid black' }}>
+                                                    <p><strong>Temp de départ</strong>: {item.timeDepart}</p>
+                                                    <p><strong>Temp d'arrivage</strong>: {item.timeArrive}</p>
+                                                    <p><strong>Station de départ</strong>: {item.stationDepart}</p>
+                                                    <p><strong>Station d'arrivage</strong>: {item.stationArrive} </p>
+                                                    <p><strong>Prix</strong>: {item.cost} </p>
+                                                </div>
+
+                                            ))
+                                        }
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button onClick={this.closeModal.bind(this)}>Close</Button>
+                                    </Modal.Footer>
+                                </Modal>
                             </div>
                         </div>
                     </div>
